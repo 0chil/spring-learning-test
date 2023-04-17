1d7b790f99f50904e613cf134f04d7e2096e4cfa
0chil <0@chll.it>
2023-04-17 15:52:49 +0900

    feat(SimpleInsertDao): SimpleJdbcInsert 학습

--- src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
+++ src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
@@ -1,6 +1,10 @@
 package nextstep.helloworld.jdbc.simpleinsert;
 
 import nextstep.helloworld.jdbc.Customer;
+
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -22,7 +26,22 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithMap(Customer customer) {
-        return null;
+        // SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+        //         .addValue("first_name", customer.getFirstName())
+        //         .addValue("last_name", customer.getLastName());
+        // SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+        //         .addValue("first_Name", customer.getFirstName())
+        //         .addValue("last_Name", customer.getLastName());
+        // SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+        //         .addValue("firstName", customer.getFirstName())
+        //         .addValue("lastName", customer.getLastName());
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue("firstNAME", customer.getFirstName())
+                .addValue("lastNAME", customer.getLastName());
+
+        long key = insertActor.executeAndReturnKey(sqlParameterSource).longValue();
+        // AbstractJdbcInsert가 TableMetaDataContext를 사용해 대소문자, camelcase-snakecase 모두 고려하여 알아서 넣어준다.
+        return new Customer(key, customer.getFirstName(), customer.getLastName());
     }
 
     /**
@@ -31,6 +50,9 @@ public class SimpleInsertDao {
      * id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithBeanPropertySqlParameterSource(Customer customer) {
-        return null;
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(customer);
+
+        long key = insertActor.executeAndReturnKey(sqlParameterSource).longValue();
+        return new Customer(key, customer.getFirstName(), customer.getLastName());
     }
 }
